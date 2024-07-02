bd28dc6b9d3c9d60a2adc913d4b6fc192c442820
sravanirenangi <sravanirenangi99@gmail.com>
2024-07-02 14:04:15 +0530

    react concepts that I learned are added

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import './App.css';
+import Counter from './components/counter/Counter';
+import LocalStorage from './components/local-storage/LocalStorage';
+import { JSConcepts } from './JS-concepts/JSConcepts';
+import CheckBox from './components/checkbox/CheckBox';
+import Dropdown from './components/dropdown/Dropdown';
+import Table from './components/table/Table';
+import { UseContextHook } from './hooks/default-hooks/useContextHook';
+import { UseMemoHook } from './hooks/default-hooks/useMemoHook';
+import { UseRefHook } from './hooks/default-hooks/useRefHook';
+import { UseReducerHook } from './hooks/default-hooks/useReducerHook';
+import UnControlledComponent from './components/controlledanduncontrolled/UnControlledComponent';
+import ControlledComponent from './components/controlledanduncontrolled/ControlledComponent';
+import { ToDoList } from './components/todolist/ToDoList';
+
+function App() {
+  useEffect(() => {
+    JSConcepts();
+  }, []);
+  return (
+    <div>
+      <h1 className='header'>React Concepts</h1>
+      <LocalStorage/>
+      <Counter />
+      <Dropdown/>
+      <CheckBox/>
+      <Table/>
+      <UseContextHook/>
+      <UseMemoHook/>
+      <UseRefHook/>
+      <UseReducerHook/>
+      <UnControlledComponent/>
+      <ControlledComponent/>
+      <ToDoList/>
+    </div>
+  );
+}
+
+export default App;
